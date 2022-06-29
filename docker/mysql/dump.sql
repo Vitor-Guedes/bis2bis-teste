2eef88a0a349afb5a48b665f67c9a5f4e07d85a3
vitor <you@example.com>
2022-06-29 19:38:33 -0300

    ambiente docker

---
+++ docker/mysql/dump.sql
@@ -0,0 +1,28 @@
+create database if not exists `blog`;
+
+use `blog`;
+
+create table if not exists `admin_user` (
+    `id` int primary key auto_increment,
+    `username` varchar (150) not null,
+    `password` varchar (150) not null
+)engine=innodb;
+
+create table if not exists `visitor_user` (
+    `id` int primary key auto_increment,
+    `username` varchar (150) not null,
+    `email` varchar (150) not null,
+    `password` varchar (150) not null,
+    `active` int default 1
+)engine=innodb;
+
+create table if not exists `post` (
+    `id` int primary key auto_increment,
+    `user_id` int,
+    `title` varchar (150) not null,
+    `content` varchar (150) not null,
+    `created_at` timestamp default now(),
+    `updated_at` timestamp nullable,
+    `publish` int default 1,
+    foreign key (`user_id`) references `visitor_user` (`id`)
+)engine=innodb;
